6449c66b3562c53ff121002189fc72e848dff9eb
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2023-06-16 10:38:36 +0200

    fixed publish workflow

--- setup.py
+++ setup.py
@@ -1,9 +1,48 @@
 from distutils.core import setup
 import os
-from conf_version_file import conf_version
 from setuptools import find_packages
+import subprocess
+import sys
 
-VERSION = "0.3.2"
+VERSION = "0.3.3"
+
+# conf version file
+def conf_version(in_file_name, cmake_version, out_file_name):
+    def run_command(cmd):
+        return subprocess.check_output(cmd)
+
+    git_commit_hash="-".join(run_command(["git", "log", "-1", "--format=%h-%ci"]).decode().split()[:2])
+    status_out = run_command(["git", "status", "-s"])
+    if len(status_out) > 0:
+        print("WARNING: building on dirty git repo", status_out)
+        git_status="D-"
+    else:
+        git_status=""
+
+    os.makedirs(os.path.dirname(out_file_name), exist_ok=True)
+
+    # configure the new version file
+    out_lines = []
+    with open(in_file_name, "r") as in_file:
+        for line in in_file.readlines():
+            line = line.replace("@CMAKE_VERSION@", cmake_version)
+            line = line.replace("@GIT_COMMIT_HASH@", git_commit_hash)
+            line = line.replace("@GIT_STATUS@", git_status)
+            out_lines.append(line)
+
+    file_changed = False
+    if not os.path.isfile(out_file_name):
+        file_changed=True
+    else:
+        with open(out_file_name, "r") as in_file:
+            lines = in_file.readlines()
+            file_changed = lines == out_lines
+
+    if file_changed:
+        print("writing new version file")
+        with open(out_file_name, "w") as out_file:
+            for line in out_lines:
+                out_file.write(line)
 
 # update version file...
 conf_version("VERSION.in", VERSION, "biosmoother/VERSION")
