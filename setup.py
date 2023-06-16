3ce8bfd51c978be0287b8b9589c2f84b9b5ff2a9
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2023-06-16 11:32:47 +0200

    bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages
 import subprocess
 
-VERSION = "0.3.3"
+VERSION = "0.3.4"
 
 # conf version file
 def conf_version(in_file_name, cmake_version, out_file_name):
