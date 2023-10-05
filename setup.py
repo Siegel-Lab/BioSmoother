9149193dfff5c5e2ca70186ede4a51d2042b2d93
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2023-10-05 13:29:15 +0200

    intermediate

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ def package_files(directory):
             paths.append(os.path.join('..', path, filename))
     return paths
 extra_files = package_files('biosmoother')
-print(extra_files)
 
 setup(
     name="biosmoother",
