6d16e6cc42c9f4c93882b06c1decf2fe25a63b39
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2024-01-11 17:42:16 +0100

    bump version to 1.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import subprocess
 # from libbiosmoother import open_descriptions_json
 import json
 
-VERSION = "1.3.1"
+VERSION = "1.3.2"
 
 # conf version file
 def conf_version(in_file_name, cmake_version, out_file_name):
